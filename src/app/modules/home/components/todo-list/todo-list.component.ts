b42fc1870054af7f0fc3224fc2df2e045edcc85a
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-05-31 11:32:35 -0300

    Itens armazenados no local storage

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,12 +10,19 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   constructor() { }
 
   ngDoCheck() {
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorage();
+  }
+
+  public setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
   }
 
   public setEmitTaskList(event: string) {
@@ -35,6 +42,7 @@ export class TodoListComponent implements DoCheck {
   }
 
   public validationInput(event: string, index: number) {
+    event = event.trim();
     if (!event.length) {
       const confirm = window.confirm("Task está vazia, deseja deletar?");
 
